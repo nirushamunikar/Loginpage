09ea054fa891d0648bf85329455607d1f9141223
Nirusha Munikar <nirushamunikar26@gmail.com>
2017-05-29 19:48:09 +0545

    Update login.php

--- login.php
+++ login.php
@@ -3,10 +3,10 @@
 <body>
 <form action="check.php" method="post">
     <label>Username</label>
-        <input type="text" name="username" value=""><br><br>
-     <label>Password</label>   
-        <input type="password" name="password" value=""><br>
-        <input type="submit" value="login">
+    <input type="text" name="username" value=""><br><br>
+    <label>Password</label>   
+    <input type="password" name="password" value=""><br>
+    <input type="submit" value="login">
 
 </form>
 
